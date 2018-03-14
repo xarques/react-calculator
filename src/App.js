6854f2a11983e3f675c60fa8df720457e4c71128
Caleb Pollman <cpollman1@gmail.com>
2018-03-13 19:20:45 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React, {Component} from 'react';
-import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        Hello World!
-      </div>
-    );
-  }
+const App () => {
+  return (
+    <div className="App">
+      Hello World!
+    </div>
+  );
 }
 
 export default App;
