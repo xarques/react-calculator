4f264ae8f2cd5fcbf0bfd1e6534d1bb0e27dc92a
Xavier Arques <xavierarques@yahoo.com>
2018-08-05 18:46:45 +0200

    Write the first test that fails

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 
 ReactDOM.render(
-  <div>Hello World!</div>, 
+  <div>Hello World!</div>,
   document.getElementById('root')
 );
