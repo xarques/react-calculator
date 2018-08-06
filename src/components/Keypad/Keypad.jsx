ef4e08417df5c4355cf03210d97b27fe5cb8ca54
Xavier Arques <xavierarques@yahoo.com>
2018-08-06 18:05:16 +0200

    Tests + Circle Ci

--- src/components/Keypad/Keypad.jsx
+++ src/components/Keypad/Keypad.jsx
@@ -1,8 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Keypad.css';
 
 const Keypad = ({callOperator, numbers, operators, setOperator, updateDisplay}) => {
-  return (<div className="keypad-container" />);
+  const displayedNumbers = numbers.map(number => {
+    return (
+      <p key={number}>{number}</p>
+    )
+  });
+
+  const displayedOperators = operators.map(operator => {
+    return (
+      <p key={operator}>{operator}</p>
+    )
+  });
+
+  return (
+    <div className="keypad-container">
+      <div className="numbers-container">
+        {displayedNumbers}
+      </div>
+      <div className="operators-container">
+        {displayedOperators}
+      </div>
+    </div>
+  );
 }
 
 Keypad.propTypes = {
